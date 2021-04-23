75e732ced2561fcef8d62e09ed1b5ad03a279d05
github username <schacter.jon@gmail.com>
2021-04-23 14:06:35 -0700

    convert to class component

--- src/components/recipe/RecipeBinder.js
+++ src/components/recipe/RecipeBinder.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-const RecipeBinder = ({ user }) => {
-    return(
-        <div>
-            { user.binder_name ? <h2>{user.binder_name}</h2> : <h2>MY BINDER</h2> }
-        </div>
-    )
+class RecipeBinder extends Component {
+    render(){
+        const { user } = this.props
+        let name
+
+        if (user && user.binder_name) {
+            name = user.binder_name
+        } else {
+            name = "My Binder"
+        }
+        
+        return(
+            <div className="recipe-binder">
+                <h1>{name}</h1>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = ({ currentUser }) => {
