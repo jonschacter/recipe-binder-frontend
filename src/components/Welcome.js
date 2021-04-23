e56e72a99adba1ad9635fa0807662bb4eb04132a
github username <schacter.jon@gmail.com>
2021-04-23 13:22:31 -0700

    add conditional logout link

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Welcome = () => {
+import { logout } from '../actions/currentUser.js'
+
+const Welcome = ({ loggedIn, logout, history }) => {
     return (
         <div>
             <h2>Welcome</h2>
-            <p>Please <Link to="/login">Log In</Link> or <Link to="/signup">Sign Up</Link>.</p>
+            { loggedIn ? <p><Link onClick={ () => logout(history) }>Log Out</Link></p> : <p>Please <Link to="/login">Log In</Link> or <Link to="/signup">Sign Up</Link>.</p>}
         </div>
     )
 }
 
-export default Welcome
+const mapStateToProps = ({ currentUser }) => {
+    return {
+        loggedIn: !!currentUser
+    }
+}
+
+export default connect(mapStateToProps, { logout })(Welcome)
