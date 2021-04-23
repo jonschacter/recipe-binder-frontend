94f9ad5f665e01a8f3b3f6fdae3f1a895de0b1b6
github username <schacter.jon@gmail.com>
2021-04-23 12:28:43 -0700

    create '/' welcome component with links to log in or sign up

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import './App.css';
 
+import Welcome from './components/Welcome.js'
+
 class App extends Component {
-    
+
     render(){
         return(
             <div className="App">
                 <h1>RECIPE BINDER</h1>
                 <Router>
                     <Switch>
-                        
+                        <Route exact path="/" component={Welcome} />
                     </Switch>
                 </Router>
             </div>
