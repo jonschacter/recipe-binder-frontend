53136070a272b4e367e797b73c46e80410a0db28
github username <schacter.jon@gmail.com>
2021-04-23 13:42:46 -0700

    remove Log Out - moved to Navbar

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,10 +5,14 @@ import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser.js'
 
 const Welcome = ({ loggedIn, logout, history }) => {
+    const redirect = () => {
+        history.push('/recipes')
+    }
+    
     return (
         <div>
             <h2>Welcome</h2>
-            { loggedIn ? <p><Link onClick={ () => logout(history) }>Log Out</Link></p> : <p>Please <Link to="/login">Log In</Link> or <Link to="/signup">Sign Up</Link>.</p>}
+            { loggedIn ? redirect() : <p>Please <Link to="/login">Log In</Link> or <Link to="/signup">Sign Up</Link>.</p>}
         </div>
     )
 }
