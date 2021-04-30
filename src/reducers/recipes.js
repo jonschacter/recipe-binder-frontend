a296717dac1174fbd8cb80d303879ecfe20da0de
github username <schacter.jon@gmail.com>
2021-04-30 13:23:32 -0700

    create recipes reducer with SET_RECIPES logic

---
+++ src/reducers/recipes.js
@@ -0,0 +1,10 @@
+const recipes = (state = [], action) => {
+    switch(action.type) {
+        case "SET_RECIPES":
+            return action.recipes
+        default:
+            return state
+    }
+}
+
+export default recipes
