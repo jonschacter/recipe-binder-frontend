5fc0b2ca9f04d0a6ae2e8cbd58413bfb3dc372cf
github username <schacter.jon@gmail.com>
2021-04-30 14:09:21 -0700

    create EditCategories component

---
+++ src/components/category/EditCategories.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EditCategories extends Component {
+    render(){
+        return(
+            <div className="edit-categories">
+                EDIT CATEGORIES
+            </div>
+        )
+    }
+}
+
+export default EditCategories
