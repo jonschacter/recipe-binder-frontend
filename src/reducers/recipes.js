c46e4c12be9641c77de97d9247f089aa5adc7778
github username <schacter.jon@gmail.com>
2021-04-30 18:13:11 -0700

    create response for UPDATE_CATEGORY

--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -2,6 +2,9 @@ const recipes = (state = [], action) => {
     switch(action.type) {
         case "SET_RECIPES":
             return action.recipes
+        case "UPDATE_CATEGORY":
+            const index = state.findIndex(cat => cat.id === action.category.id)
+            return [...state.slice(0, index), action.category, ...state.slice(index+1)]
         default:
             return state
     }
