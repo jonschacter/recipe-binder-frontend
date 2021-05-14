f306af319dc960cc086fdf46989346ec532d0e2c
github username <schacter.jon@gmail.com>
2021-05-14 14:19:29 -0700

    add category delete button with confirmation alert

--- src/components/category/EditCategory.js
+++ src/components/category/EditCategory.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react'
 import { Draggable } from 'react-beautiful-dnd'
 
 class EditCategory extends Component {
+    handleClick = (e) => {
+        console.log(`${e.target.name} - ${e.target.id}`)
+        if (window.confirm('Deleting this category will also delete any associated recipes. Are you sure you want to proceed?')) {
+            console.log("user confirmed delete")
+        }
+    }
+    
     render(){
+        const { category, index } = this.props
         return (
-            <Draggable draggableId={"" + this.props.category.id} index={this.props.index}>
+            <Draggable draggableId={"" + category.id} index={index}>
                 {(provided) => (
                     <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                        <h3>{this.props.category.name}</h3>
+                        <h3>{category.name}<button id={category.id} name={category.name} onClick={this.handleClick}>x</button></h3>
                     </div>
                 )}
-                
             </Draggable>
         )
     }
