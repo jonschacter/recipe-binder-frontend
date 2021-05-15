0cd4891d4c56d9d643ca4ef507e91cad8bac2c74
github username <schacter.jon@gmail.com>
2021-05-14 18:26:40 -0700

    fix REMOVE_CATEGORY logic

--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -6,8 +6,7 @@ const recipes = (state = [], action) => {
             const indexToUpdate = state.findIndex(cat => cat.id === action.category.id)
             return [...state.slice(0, indexToUpdate), action.category, ...state.slice(indexToUpdate+1)]
         case "REMOVE_CATEGORY":
-            const indexToRemove = state.findIndex(cat => cat.id === action.categoryId)
-            return [...state.slice(0, indexToRemove), ...state.slice(indexToRemove+1)]
+            return state.filter(cat => cat.id !== parseInt(action.categoryId))
         default:
             return state
     }
